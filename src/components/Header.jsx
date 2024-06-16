ee2865d90a495cc74d35f94699020e4538c0cef3
Daniel Silva <daniel@c1risk.com>
2024-06-15 22:43:35 -0300

    global style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 export function Header() {
     return (
         <header className={styles.header} >
-            Ignete Feed
+            Ignite Feed
         </header>
     );
 }
