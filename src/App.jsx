70b65719d9c34fdec1ccac0c095299333afa4639
Daniel Tavares <dtavares@atlassian.com>
2024-06-17 08:12:14 -0300

    sidebar logo + button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar";
+import styles from "./App.module.css"
 
 import './global.css';
 
@@ -7,6 +9,22 @@ function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+
+        <Sidebar/>
+        <main>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Beatae in explicabo optio quisquam eius voluptas neque quidem recusandae velit laborum veritatis sapiente veniam accusamus, 
+          quo alias dicta, vitae magnam harum.
+
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+          Beatae in explicabo optio quisquam eius voluptas neque quidem recusandae velit laborum veritatis sapiente veniam accusamus, 
+          quo alias dicta, vitae magnam harum.
+        </main>
+
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar} >
+            <img
+                className={styles.cover} 
+                src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+            />
+
+            <div className={styles.profile} >
+                <strong>Daniel T.</strong>
+                <span>Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar Perfil
+                </a>
+            </footer>
+
+        </aside>
+    );
+}
